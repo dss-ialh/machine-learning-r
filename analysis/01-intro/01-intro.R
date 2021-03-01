f919d2fe878d39512a5688614862aabe7fbde84a
Kyle Belanger <kyleb44@hotmail.com>
2021-03-01 17:30:54 -0500

    Create 01-intro.R

---
+++ analysis/01-intro/01-intro.R
@@ -0,0 +1,124 @@
+# The following script is used for EDA of the common data sets used throughout
+# the book.  It is not meant to perform any modeling but to get to know the data
+
+
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\014") # Clear the console
+
+# verify root location
+cat("Working directory: ", getwd()) # Must be set to Project Directory
+# if the line above DOES NOT generates the project root, re-map by selecting
+# Session --> Set Working Directory --> To Project Directory location
+# Project Directory should be the root by default unless overwritten
+
+# ---- load-packages -----------------------------------------------------------
+library(magrittr)  # pipes
+library(dplyr)     # data wrangling
+library(ggplot2)   # graphs
+
+
+# ---- load-sources ------------------------------------------------------------
+
+source("./scripts/common-functions.R")
+
+
+# ---- declare-globals ---------------------------------------------------------
+# custom function for HTML tables
+neat <- function(x, output_format = "html"){
+  # knitr.table.format = output_format
+  if(output_format == "pandoc"){
+    x_t <- knitr::kable(x, format = "pandoc")
+  }else{
+    x_t <- x %>%
+      # x %>%
+      # neat() %>%
+      knitr::kable(format=output_format) %>%
+      kableExtra::kable_styling(
+        bootstrap_options = c("striped", "hover", "condensed","responsive"),
+        # bootstrap_options = c( "condensed"),
+        full_width = F,
+        position = "left"
+      )
+  }
+  return(x_t)
+}
+# Note: when printing to Word or PDF use `neat(output_format =  "pandoc")`
+
+
+prints_folder <- paste0("./analysis/.../prints/", strftime(Sys.Date()))
+if(!file.exists(prints_folder)){
+  dir.create(file.path(prints_folder))
+}
+
+ggplot2::theme_set(
+  ggplot2::theme_bw(
+  )+
+    theme(
+      strip.background = element_rect(fill="grey95", color = NA)
+    )
+)
+quick_save <- function(g,name,...){
+  ggplot2::ggsave(
+    filename = paste0(name,".jpg"),
+    plot     = g,
+    device   = "jpg",
+    path     = prints_folder,
+    # width    = width,
+    # height   = height,
+    # units = "cm",
+    dpi      = 'retina',
+    limitsize = FALSE,
+    ...
+  )
+}
+# ---- load-data ---------------------------------------------------------------
+
+# AMES HOUSING
+# access data
+ames <- AmesHousing::make_ames()
+
+# initial dimension
+dim(ames)
+## [1] 2930   81
+
+# response variable
+head(ames$Sale_Price)
+
+
+# Employee Attrition
+# Note this is different from the book as the data has moved
+
+data("attrition", package = "modeldata")
+
+# initial dimension
+dim(attrition)
+## [1] 1470   31
+
+# response variable
+head(attrition$Attrition)
+
+
+
+
+# ---- tweak-data --------------------------------------------------------------
+
+
+# ---- table-1 -----------------------------------------------------------------
+
+# ---- graph-1 -----------------------------------------------------------------
+
+
+# ---- graph-2 -----------------------------------------------------------------
+
+
+# ----- publish ----------------------------------------------------------------
+path <- "./analysis/report/report-isolated.Rmd"
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # "word_document"
+    # "pdf_document"
+  ),
+  clean=TRUE
+)
