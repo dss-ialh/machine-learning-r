137ad9512a268722adf911377aa298754a06c599
Andriy V. Koval <koval.andrey@gmail.com>
2021-04-15 14:15:09 -0400

    Create 05-01-heart.R

---
+++ analysis/05-logistic-regression/05-01-heart.R
@@ -0,0 +1,125 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\014") # Clear the console
+
+# verify root location
+cat("Working directory: ", getwd()) # Must be set to Project Directory
+# if the line above DOES NOT generates the project root, re-map by selecting
+# Session --> Set Working Directory --> To Project Directory location
+# Project Directory should be the root by default unless overwritten
+
+# ---- load-packages -----------------------------------------------------------
+library(magrittr)  # pipes
+library(dplyr)     # data wrangling
+library(ggplot2)   # graphs
+library(janitor)   # tidy data
+library(tidyr)     # data wrangling
+library(forcats)   # factors
+library(stringr)   # strings
+library(lubridate) # dates
+library(caret) # cross-validation
+library(vip) # variable importance
+# ---- load-sources ------------------------------------------------------------
+
+source("./scripts/common-functions.R")
+
+
+# ---- declare-globals ---------------------------------------------------------
+# custom function for HTML tables
+neat <- function(x, output_format = "html"){
+  # knitr.table.format = output_format
+  if(output_fo...rmat == "pandoc"){
+    x_t <- knitr::kable(x, format = "pandoc")
+  }else{
+    x_t <- x %>%
+      # x %>%
+      # neat() %>%
+      knitr::kable(format=output_format) %>%
+      kableExtra::kable_styling(
+        bootstrap_options = c("striped", "hover", "condensed","responsive"),
+        # bootstrap_options = c( "condensed"),
+        full_width = F,
+        position = "left"
+      )
+  }
+  return(x_t)
+}
+# Note: when printing to Word or PDF use `neat(output_format =  "pandoc")`
+
+
+prints_folder <- paste0("./analysis/04-linear-regression/prints/", strftime(Sys.Date()))
+if(!file.exists(prints_folder)){
+  dir.create(file.path(prints_folder))
+}
+
+ggplot2::theme_set(
+  ggplot2::theme_bw(
+  )+
+    theme(
+      strip.background = element_rect(fill="grey95", color = NA)
+    )
+)
+quick_save <- function(g,name,...){
+  ggplot2::ggsave(
+    filename = paste0(name,".jpg"),
+    plot     = g,
+    device   = "jpg",
+    path     = prints_folder,
+    # width    = width,
+    # height   = height,
+    # units = "cm",
+    dpi      = 'retina',
+    limitsize = FALSE,
+    ...
+  )
+}
+
+# data source: https://www.kaggle.com/yamaerenay/world-happiness-report-preprocessed
+path <- "./data-public/raw/heart/heart.csv"
+
+# ---- load-data ---------------------------------------------------------------
+ds0 <- readr::read_csv(path)
+
+# ---- tweak-data --------------------------------------------------------------
+
+names_lables <- c(
+   "age"     = 'Age'
+  ,"sex"     = 'Sex'
+  ,"cp"      = 'Chest Pain Type'
+  ,"trtbps"  = 'Resting Blood Pressure'
+  ,"chol"    = 'Cholesterol'
+  ,"fbs"     = 'Fasting Blood Sugar'
+  ,"restecg" = 'Resting ECG'
+  ,"thalachh"= 'Max. HR Acheived'
+  ,"exng"    = 'Exercise Induced Angina'
+  ,"oldpeak" = 'ST Depression'
+  ,"slp"     = 'ST Slope'
+  ,"caa"     = 'Num. Major Blood Vessels'
+  ,"thall"   = 'Thalassemia'
+  ,"output"  = 'Heart Attack'
+
+)
+
+
+# ----- inspect-data -------
+
+ds0 %>% glimpse()
+
+
+# ---- table-1 -----------------------------------------------------------------
+
+# ---- graph-1 -----------------------------------------------------------------
+s
+# ---- graph-2 -----------------------------------------------------------------
+
+
+# ----- publish ----------------------------------------------------------------
+path <- "./analysis/report/report-isolated.Rmd"
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # "word_document"
+    # "pdf_document"
+  ),
+  clean=TRUE
+)
